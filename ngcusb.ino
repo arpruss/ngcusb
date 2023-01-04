c8cfff4618dcbd9579c23982359fcdc048a9b7a5
arpruss <arpruss@gmail.com>
2023-01-03 20:57:17 -0600

    foo

--- ngcusb.ino
+++ ngcusb.ino
@@ -2,7 +2,7 @@
 #include "GameControllers.h"
 #include <USBComposite.h>
 
-// left:XBox, right:padjoy, down:POWERPADRIGHT, up:arrows,space,backspace,+,-
+// left:XBox, right:padjoy, down:power pad right, up:power pad left
 
 #define LED PC13
 // Facing GameCube socket (as on console), flat on top:
@@ -15,14 +15,13 @@
 // GameCube 2--1Kohm--3.3V
 // GameCube 3--GND
 // GameCube 4--GND
-// GameCube 6--3.3V
 
 const uint32_t watchdogSeconds = 4;
 
 #define PRODUCT_ID_JOYSTICK 0x7E47
 #define PRODUCT_ID_KEYBOARD 0x7E48
 
-enum { MODE_POWERPADRIGHT = 0, MODE_XBOX, MODE_PADJOY, MODE_ARROWS };
+enum { MODE_POWERPADRIGHT = 0, MODE_XBOX, MODE_PADJOY, MODE_POWERPADLEFT };
 enum { USB_J, USB_K, USB_XBOX };
 const uint8_t usbMode[] = { USB_K, USB_XBOX, USB_J, USB_K };
 uint32_t mode = MODE_POWERPADRIGHT;
@@ -56,7 +55,7 @@ const uint16_t gcmaskL = 0x4000;
  * -- 10 --
  * 
  * left pad:
- * bs -- en
+ * z  -- q
  * z  a  q
  * x  -- w
  * -- d  --
@@ -73,9 +72,9 @@ const uint16_t gcmaskL = 0x4000;
 #define BS KEY_BACKSPACE
 
                        //     00   01   02   03   04   05   06   07   08   09   10   11   12   13   14   15
-const uint8_t ppRight[16] = { 'v', 'r', 0,   0,   ' ', 0,   0,   0,   'e', 'c', 's', 'f', TAB, 0,   0,   0 };
-const uint8_t ppLeft[16]  = { 'q', 'z', 0,   0,   EN,  0,   0,   0,   'x', 'w', 'd', 'a', BS,  0,   0,   0 };
-const uint8_t arrows[16] =  { ' ', BS, '[',  ']', '=', 0,   0,   0, KEY_LEFT_ARROW, KEY_RIGHT_ARROW, KEY_DOWN_ARROW, KEY_UP_ARROW, '-', 0,0  };
+const uint8_t ppRight[16] = { 'v', 'r', 0,   0,   ']', 0,   0,   0,   'e', 'c', 's', 'f', '[', 0,   0,   0 };
+const uint8_t ppLeft[16]  = { 'q', 'z', 0,   0,   '=',  0,   0,   0,   'x', 'w', 'd', 'a', '-',  0,   0,   0 };
+//const uint8_t arrows[16] =  { ' ', BS, '[',  ']', '=', 0,   0,   0, KEY_LEFT_ARROW, KEY_RIGHT_ARROW, KEY_DOWN_ARROW, KEY_UP_ARROW, '-', 0,0  };
 const uint8_t joy[16] = { 1, 2, 3, 4, 5, 0,0,0, 6,7,8,9, 10,11,12,0 };
 const uint8_t xbuttons[16] = { XBOX_A, XBOX_B, XBOX_X, XBOX_Y, XBOX_START, 0,0,0, XBOX_DLEFT, XBOX_DRIGHT, XBOX_DDOWN, XBOX_DUP, XBOX_RSHOULDER, XBOX_R3, XBOX_L3 }; 
 
@@ -120,7 +119,7 @@ void endMode() {
     XBox360.buttons(0);
     XBox360.send();
   }
-  else if (mode == MODE_POWERPADRIGHT || mode == MODE_ARROWS) {
+  else if (mode == MODE_POWERPADRIGHT || mode == MODE_POWERPADLEFT) {
     keyboard.releaseAll();
     for (uint32_t i = 0 ; i < 16 ; i++)
       pressed[i] = false;
@@ -174,8 +173,8 @@ void emit(GameControllerData_t* d) {
       
       XBox360.send();
     }
-    else if (mode == MODE_POWERPADRIGHT || mode == MODE_ARROWS) {
-      const uint8_t* keyMap = mode == MODE_POWERPADRIGHT ? ppRight : arrows;
+    else if (mode == MODE_POWERPADRIGHT || mode == MODE_POWERPADLEFT) {
+      const uint8_t* keyMap = mode == MODE_POWERPADRIGHT ? ppRight : ppLeft;
       //joystick.sendReport(); // just in case
       uint32_t mask = 1;
       for (uint32_t i = 0 ; i < 16 ; i++,mask<<=1) {
@@ -243,7 +242,7 @@ void loop() {
         newMode = MODE_POWERPADRIGHT;
       }
       else if ((data.buttons & gcmaskDUp)) {
-        newMode = MODE_ARROWS;
+        newMode = MODE_POWERPADLEFT;
       }
       if (newMode != mode) {
         digitalWrite(LED,1);
