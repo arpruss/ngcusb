4511f4f02ee8c77a020f02d8b4d5dba8bcdb9703
arpruss <arpruss@gmail.com>
2023-01-05 18:05:57 -0600

    notes

--- ngcusb.ino
+++ ngcusb.ino
@@ -2,6 +2,25 @@
 #include "GameControllers.h"
 #include <USBComposite.h>
 
+//timing: mat: 401us
+//usb 24ms
+//usb at 4: 
+//xbox 8ms
+
+//#define TEST
+
+#define MAX_CONTROLLERS 1
+
+#ifdef TEST
+#define TEST1 PB12
+#define TEST2 PB13
+#define TEST1WRITE(x) digitalWrite(TEST1,(x))
+#define TEST2WRITE(x) digitalWrite(TEST2,(x))
+#else
+#define TEST1WRITE(x) 
+#define TEST2WRITE(x)
+#endif
+
 // left:XBox, right:padjoy, down:power pad right, up:power pad left
 
 #define LED PC13
@@ -21,12 +40,20 @@ const uint32_t watchdogSeconds = 4;
 #define PRODUCT_ID_JOYSTICK 0x7E47
 #define PRODUCT_ID_KEYBOARD 0x7E48
 
+const uint32_t maximumPollingRateMicroseconds=2500;
+uint32_t lastPollTime = 0;
 enum { MODE_POWERPADRIGHT = 0, MODE_XBOX, MODE_PADJOY, MODE_POWERPADLEFT };
 enum { USB_J, USB_K, USB_XBOX };
 const uint8_t usbMode[] = { USB_K, USB_XBOX, USB_J, USB_K };
 uint32_t mode = MODE_POWERPADRIGHT;
 
-GameCubeController gc(PA6);
+GameCubeController gc1(PA6);
+#if MAX_CONTROLLERS == 1
+GameCubeController* controllers[MAX_CONTROLLERS] = { &gc1 };
+#else
+GameCubeController gc2(PA7);
+GameCubeController* controllers[MAX_CONTROLLERS] = { &gc1, &gc2 };
+#endif
 USBXBox360 XBox360;
 USBHID HID;
 HIDKeyboard keyboard(HID);
@@ -88,6 +115,7 @@ void startUSBMode() {
   else if (usbMode[mode] == USB_J) {
     USBComposite.setProductString("NGC pad to joystick");
     USBComposite.setProductId(PRODUCT_ID_JOYSTICK);  
+    HID.setTXInterval(4);
     HID.begin(HID_JOYSTICK);
     joystick.setManualReportMode(true);
     keyboard.begin();
@@ -96,6 +124,7 @@ void startUSBMode() {
   else if (usbMode[mode] == USB_K) {
     USBComposite.setProductString("NGC pad to keyboard");
     USBComposite.setProductId(PRODUCT_ID_KEYBOARD);  
+    HID.setTXInterval(4);
     HID.begin(HID_KEYBOARD);
     joystick.setManualReportMode(true);
     keyboard.begin();
@@ -138,6 +167,15 @@ void endMode() {
 */
 
 void setup() {
+#ifdef TEST
+  pinMode(TEST1,OUTPUT);
+  pinMode(TEST2,OUTPUT);
+  digitalWrite(TEST1,0);
+  digitalWrite(TEST2,0);
+#endif  
+  TEST1WRITE(0);
+  TEST2WRITE(1);
+  
   iwdg_init(IWDG_PRE_256, watchdogSeconds*156);
   pinMode(LED,OUTPUT);
   digitalWrite(LED,1);
@@ -147,17 +185,51 @@ void setup() {
     mode = MODE_PADJOY;
   else
     mode = v;
+  mode = MODE_PADJOY;
   startUSBMode();
   while(!USBComposite);
-  gc.setDPadToJoystick(false);
-  gc.begin();
+  for (uint8_t i = 0 ; i < MAX_CONTROLLERS ; i++) {
+    controllers[i]->setDPadToJoystick(false);
+    controllers[i]->begin();  
+  }
+/*  uint32 id=0;
+  
+  controllers[0]->begin();  
+  
+  controllers[0]->sendBits(0b000000001, 9);
+  if (controllers[0]->receiveBits(&id, 24)) {
+    keyboard.println((id>>16) & 0xFF, HEX);
+    keyboard.println((id>>8) & 0xFF, HEX);
+    keyboard.println((id) & 0xFF, HEX);
+  }
+  else {
+    keyboard.println("bye");
+  }
+  for (uint8_t i = 0 ; i < MAX_CONTROLLERS ; i++) {
+    controllers[i]->setDPadToJoystick(false);
+    controllers[i]->begin();  
+  }
+/*
+  while(0) {
+  controllers[0]->sendBits(0b0100000000000011000000001l, 25);
+  uint8_t d[8];
+  for (int i=0;i<8;i++) d[i]=0xFF;
+  bool success = controllers[0]->receiveBits(d, 64);
+  if (success) {
+    keyboard.println("ok");
+  }
+    for (uint8_t i=0; i<8; i++)
+      keyboard.println(d[i],HEX);
+  }
+*/
 }
+
 
 static inline int16_t range10u16s(uint16_t x) {
   return (((int32_t)(uint32_t)x - 512) * 32767 + 255) / 512;
 }
 
-void emit(GameControllerData_t* d) {
+void emit(GameControllerData_t* d, int controllerNumber) {
     if (mode == MODE_XBOX) {
       XBox360.buttons(0);
       uint16_t mask = 1;
@@ -174,7 +246,7 @@ void emit(GameControllerData_t* d) {
       XBox360.send();
     }
     else if (mode == MODE_POWERPADRIGHT || mode == MODE_POWERPADLEFT) {
-      const uint8_t* keyMap = mode == MODE_POWERPADRIGHT ? ppRight : ppLeft;
+      const uint8_t* keyMap = ((mode == MODE_POWERPADRIGHT) ^ (0 != controllerNumber)) ? ppRight : ppLeft;
       //joystick.sendReport(); // just in case
       uint32_t mask = 1;
       for (uint32_t i = 0 ; i < 16 ; i++,mask<<=1) {
@@ -208,7 +280,9 @@ void emit(GameControllerData_t* d) {
       joystick.Yrotate(d->cY);        
       joystick.sliderLeft(d->shoulderLeft);
       joystick.sliderRight(d->shoulderRight);
+      TEST2WRITE(1);
       joystick.sendReport();
+      TEST2WRITE(0);
     }
 }
 
@@ -226,46 +300,59 @@ void indicate(int n) {
 
 void loop() {
   iwdg_feed();
+
+#if MAX_CONTROLLERS == 1
+  while((uint32_t)(micros()-lastPollTime) < maximumPollingRateMicroseconds) ;
+#endif  
   
   GameControllerData_t data;
-  if (gc.read(&data)) {
-    digitalWrite(LED,0);
-    uint32_t newMode = mode;
-    if ((data.buttons & (gcmaskStart | gcmaskZ)) == (gcmaskStart | gcmaskZ)) {
-      if ((data.buttons & gcmaskDLeft)) {
-        newMode = MODE_XBOX;
-      }
-      else if ((data.buttons & gcmaskDRight)) {
-        newMode = MODE_PADJOY;
-      }
-      else if ((data.buttons & gcmaskDDown)) {
-        newMode = MODE_POWERPADRIGHT;
-      }
-      else if ((data.buttons & gcmaskDUp)) {
-        newMode = MODE_POWERPADLEFT;
-      }
-      if (newMode != mode) {
-        digitalWrite(LED,1);
-        EEPROM8_storeValue(0, newMode);
-        if (usbMode[newMode] != usbMode[mode]) {
-          delay(100);
-          nvic_sys_reset();
+  bool someSuccess = false;
+  for (uint8_t controller = 0 ; controller < MAX_CONTROLLERS ; controller++) {
+    if (controllers[controller]->read(&data)) {
+#if MAX_CONTROLLERS == 1      
+      lastPollTime = micros();
+#endif      
+      someSuccess = true;
+      uint32_t newMode = mode;
+      if ((data.buttons & (gcmaskStart | gcmaskZ)) == (gcmaskStart | gcmaskZ)) {
+        if ((data.buttons & gcmaskDLeft)) {
+          newMode = MODE_XBOX;
         }
-        else {
-          if (usbMode[newMode] == USB_K) {
-            keyboard.releaseAll();
-            for (uint32_t i=0; i<16; i++)
-              pressed[i] = false;
+        else if ((data.buttons & gcmaskDRight)) {
+          newMode = MODE_PADJOY;
+        }
+        else if ((data.buttons & gcmaskDDown)) {
+          newMode = MODE_POWERPADRIGHT;
+        }
+        else if ((data.buttons & gcmaskDUp)) {
+          newMode = MODE_POWERPADLEFT;
+        }
+        if (newMode != mode) {
+          digitalWrite(LED,1);
+          EEPROM8_storeValue(0, newMode);
+          if (usbMode[newMode] != usbMode[mode]) {
+            delay(100);
+            nvic_sys_reset();
           }
-          mode = newMode;
-          indicate(1+mode);
+          else {
+            if (usbMode[newMode] == USB_K) {
+              keyboard.releaseAll();
+              for (uint32_t i=0; i<16; i++)
+                pressed[i] = false;
+            }
+            mode = newMode;
+            indicate(1+mode);
+          }
         }
       }
+      emit(&data, controller);
     }
-    emit(&data);
   }
-  else {
-    digitalWrite(LED,1);
+  digitalWrite(LED,! someSuccess);
+  if (! someSuccess) {
+//    keyboard.print("!");
+    delay(1);
   }
 }
+
 
